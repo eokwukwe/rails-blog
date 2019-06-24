cb05bccfe161a78457f1a746549d6796e060136f
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2019-06-24 13:01:47 +0100

    Add authentication methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@
 
 # UsersController
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[edit update show]
+
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
                  .order('created_at DESC')
@@ -21,12 +23,9 @@ class UsersController < ApplicationController
     end
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = 'Account updated successfully.'
       redirect_to articles_path
@@ -36,13 +35,16 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page],
                                              per_page: 5)
                           .order('created_at DESC')
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   resources :articles
 
   get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   # post 'users', to: 'users#create'
   resources :users, expect: [:new]
 end
