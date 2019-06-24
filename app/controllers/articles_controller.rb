aeb0ff24cf46ceba981a1f181663232511a6a894
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2019-06-24 14:42:07 +0100

    Complete authentication system

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,8 @@
 # ArticlesController
 class ArticlesController < ApplicationController
   before_action :set_article, only: %i[edit update show destroy]
+  before_action :require_user, except: %i[index show]
+  before_action :require_same_user, only: %i[edit update destroy]
 
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
@@ -17,7 +19,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.user = User.last
+    @article.user = current_user
     if @article.save
       flash[:success] = 'Article created successfully'
       redirect_to article_path(@article)
@@ -45,11 +47,18 @@ class ArticlesController < ApplicationController
 
   private
 
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
   def set_article
     @article = Article.find(params[:id])
   end
 
-  def article_params
-    params.require(:article).permit(:title, :description)
+  def require_same_user
+    if current_user != @article.user
+      flash[:danger] = 'You can only edit or delete your own article'
+      redirect_to root_path
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@
 # UsersController
 class UsersController < ApplicationController
   before_action :set_user, only: %i[edit update show]
+  before_action :require_same_user, only: %i[edit update]
 
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -42,11 +43,18 @@ class UsersController < ApplicationController
 
   private
 
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
   def set_user
     @user = User.find(params[:id])
   end
 
-  def user_params
-    params.require(:user).permit(:username, :email, :password)
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = 'You can only edit your own account'
+      redirect_to root_path
+    end
   end
 end
