c15bf6c96f879a5d246d4b67135a9e0b1d788721
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2019-07-09 11:34:13 +0100

    Fixed unregistered users cannot read article

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@
 # UsersController
 class UsersController < ApplicationController
   before_action :set_user, only: %i[edit update show]
-  before_action :require_user
+  before_action :require_user, except: %i[new]
   before_action :require_same_user, only: %i[edit update]
   before_action :require_admin_and_same_user, only: %i[destroy]
 
